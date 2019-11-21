fedf3dbb0fde4cf6b494c949d092c3167b69a010
lcl22hope <lcl22hope@gmail.com>
2019-11-21 21:00:54 +0800

    add promise state

--- JS-issues/myPromise.js
+++ JS-issues/myPromise.js
@@ -1,21 +1,28 @@
+const PENDING = "pending";
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
+
 
 class MyPromise {
     constructor(executor) {
-        this.value = null
-        this.error = null
+        this.value = null;
+        this.error = null;
+        this.state = PENDING;
 
         const resolve = (value) => {
+            this.state = FULFILLED;
             this.value = value; 
         }
 
         const reject = (error) => {
+            this.state = REJECTED;
             this.error = error;
         }
         // 外层控制测试捕获
         try {
             executor(resolve, reject);
         } catch(error) {
-            reject(error)
+            reject(error);
         }
     }
     
